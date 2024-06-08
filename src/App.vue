5674130dbbdfc732e050ac22817e82ec644167a9
filip19992 <filipwlodarczyk99@gmail.com>
2024-06-08 12:25:18 +0200

    style footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
-<!-- src/App.vue -->
 <template>
   <div id="app">
     <HeaderPage />
     <router-view></router-view>
+    <footer class="footer">
+      <div class="footer-content">
+        <p>© 2024 WorkTracker. All rights reserved.</p>
+        <p>Designed and developed by [Your Company Name].</p>
+        <p>Contact Us: contact@worktracker.com | (123) 456-7890</p>
+      </div>
+    </footer>
   </div>
-  <footer>
-    © 2024 WorkTracker. All rights reserved.
-    Designed and developed by [Your Company Name].
-    Contact Us: contact@worktracker.com | (123) 456-7890
-  </footer>
 </template>
 
 <script>
@@ -22,3 +23,16 @@ export default {
 };
 </script>
 
+<style scoped>
+.footer {
+  background-color: #333;
+  color: #fff;
+  padding: 20px 0;
+  text-align: center;
+}
+
+.footer-content {
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
