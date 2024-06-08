bb9509a484cac0b712aa8a2b3f38880f34d572ec
filip19992 <filipwlodarczyk99@gmail.com>
2024-06-08 09:03:56 +0200

    Added header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <!-- src/App.vue -->
 <template>
   <div id="app">
+    <HeaderComponent />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import HeaderPage from './components/HeaderPage.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    HeaderPage
+  }
 };
 </script>
+
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,66 @@
+    <template>
+        <header class="header">
+            <nav>
+                <ul>
+                    <li>
+                        <router-link v-if="!isLoggedIn" to="/login">Login</router-link>
+                        <router-link v-if="!isLoggedIn" to="/register">Register</router-link>
+                        <router-link v-if="isLoggedIn" @click.prevent="logout">Logout</router-link>
+                    </li>
+                </ul>   
+        </nav>
+        </header>
+    </template>
+
+    <script>
+
+    export default {
+    data() {
+        return {
+        errorMessage: ''
+        };
+    },
+    computed: {
+        isLoggedIn() {
+        return !!localStorage.getItem('token');
+        }
+    },
+    methods: {
+        logout() {
+        localStorage.removeItem('token');
+        this.$router.push('/login');
+        }
+    }
+    };
+    </script>
+    <style>
+    .header {
+    background-color: #f8f9fa;
+    padding: 1rem;
+    border-bottom: 1px solid #ccc;
+    }
+
+    .header nav ul {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    gap: 1rem;
+    }
+
+    .header nav ul li {
+    display: inline;
+    }
+
+    .header nav ul li a {
+    text-decoration: none;
+    color: #007bff;
+    }
+
+    .header nav ul li a:hover {
+    text-decoration: underline;
+    }
+
+    .error {
+    color: red;
+    }
+    </style>
